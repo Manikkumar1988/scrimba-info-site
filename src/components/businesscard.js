bf892bf13916de7241fb4645ba6b7d3ec4c0ca50
Manikkumar <manikkumar@Manikkumars-MacBook-Pro.local>
2021-11-23 08:58:47 +0530

    adding styles

--- src/components/businesscard.js
+++ src/components/businesscard.js
@@ -1,29 +1,49 @@
 import React from 'react'
 import './businesscard.css'
 
-const BusinessCard = () => {
+const Info = () => {
     return (
-        <div className="main">
+        <div>
             <img
                 src="images/Rectangle.png"
                 alt="profile pic in business card"
             />
-            <div>Laura Smith</div>
-            <div>Frontend Developer</div>
-            <div>laurasmith.website</div>
+            <h1 className="info--name">Laura Smith</h1>
+            <h4 className="info--job-title">Frontend Developer</h4>
+            <div className="info--contact--site">laurasmith.website</div>
             <div className="contact">
                 <button>Email</button>
                 <button>LinkedIn</button>
-                <h6>About</h6>
-                <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.
-                </p>
-                <h6>Interests</h6>
-                <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
-                </p>
             </div>
-            <div>
-                <div className="footer">
-                    <div className="columns"></div>
+        </div>
+    )
+}
+
+const About = () => {
+    return (
+        <div>
+            <p className="about--title">About</p>
+            <p className="about--content">I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.
+            </p>
+        </div>
+    )
+}
+
+const Interests = () => {
+    return (
+        <div>
+            <p className="about--title">Interests</p>
+            <p className="about--content">Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+            </p>
+        </div>
+    )
+}
+
+const Footer = () => {
+    return (
+        <div>
+            <div className="footer">
+                <div className="columns">
                     <img
                         className="column"
                         src="images/twitter.svg"
@@ -42,6 +62,16 @@ const BusinessCard = () => {
                     />
                 </div>
             </div>
+        </div>
+    )
+}
+const BusinessCard = () => {
+    return (
+        <div className="main">
+            <Info />
+            <About />
+            <Interests />
+            <Footer />
         </div>
     )
 }
